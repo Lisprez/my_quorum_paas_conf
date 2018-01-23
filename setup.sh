276594c986193f04558daa492be0b9991bad3481
LispreTxd <lispre@outlook.com>
2018-01-23 14:31:58 +0800

    change setup.sh to satisfiy my interface request

--- setup.sh
+++ setup.sh
@@ -7,10 +7,28 @@
 #     echo ${args[$index]};
 # done
 
+# initialize the script environment
+
+./cleanup.sh
+
+uid=`id -u`
+gid=`id -g`
+pwd=`pwd`
+
+function echo_yellow() {
+    local_text=$1
+    echo -e "\033[33m $local_text \033[0m"
+}
+
+function echo_green() {
+    local_text=$1
+    echo -e "\033[32m $local_text \033[0m"
+}
+
 # create the directory tree for every node
 # @1 \the number of nodes
-function create_nodes_directory() {
-    echo '[1] Create directories for '$1' nodes.'
+function create_nodes_directory() { 
+    echo_green '[1] Create directories for '$1' nodes.'
     for node_index in $(seq 1 $1); do
         qd=qdata_$node_index
         mkdir -p $qd/{logs,keys}
@@ -21,18 +39,19 @@ function create_nodes_directory() {
 # create static_nodes.json file for every node
 # @1 \the number of nodes
 function create_static_nodes_json() {
-    echo '[2] Create Enodes and store them in static-nodes.json'
+    local_node_num=$1
+    echo_green '[2] Create Enodes and store them in static-nodes.json'
 
     echo "[" > static-nodes.json
 
-    for node_index in $(seq 1 $1); do
+    for node_index in $(seq 1 $local_node_num); do
         qd=qdata_$node_index
 
         # Generate the node's Enode
         enode=`docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/bootnode -genkey /qdata/dd/nodekey -writeaddress`
 
         # Append enode to the static-nodes.json file
-        sep=`[[ $n < $nnodes ]] && echo ","`
+        sep=`[[ $node_index < $local_node_num ]] && echo ","`
         echo '  "enode://'$enode'@'$ip':30303?discport=0"'$sep >> static-nodes.json
     done
     echo "]" >> static-nodes.json
@@ -41,7 +60,9 @@ function create_static_nodes_json() {
 # create genesis.json file and account
 # @1 \the number of nodes
 function create_account_and_genesis() {
-    echo '[3] Creating accounts and genesis.json file.'
+    local_node_num=$1
+
+    echo_green '[3] Creating accounts and genesis.json file.'
 
     cat > genesis.json <<EOF
 {
@@ -56,7 +77,7 @@ EOF
         account=`docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/geth --datadir=/qdata/dd --password /qdata/passwords.txt account new | cut -c 11-50`
 
         # Add the account to the genesis block so that it have some Ether initialized.
-        sep=`[[ $n < $nnodes ]] && echo ","`
+        sep=`[[ $node_index < $local_node_num ]] && echo ","`
         cat >> genesis.json <<EOF
         "${account}": {
             "balance": "1000000000000000000000000000"
@@ -84,26 +105,28 @@ EOF
 # create node list for tm.conf and finish configure all the nodes
 # @1 \the ip array that contains all the ips of all nodes
 function finish_configure_nodes() {
-    ips=$1
+    local_ips=$1
     nodelist=
     n=1
-    for ip in ${ips[@]}
+    for ip in ${local_ips[@]}
     do
-        sep=`[[ $ip != ${ips[0]} ]] && echo ","`
+        sep=`[[ $ip != ${local_ips[0]} ]] && echo ","`
         nodelist=${nodelist}${sep}'"http://'${ip}':9000/"'
         let n++
     done
 
-    echo '[4] Creating Quorum keys and finishing configuration.'
+    echo_green '[4] Creating Quorum keys and finishing configuration.'
 
     n=1
-    for ip in ${ips[*]}
+    for ip in ${local_ips[*]}
     do
         qd=qdata_$n
+        
+        pwd
 
         cat templates/tm.conf \
-            | sed s/_NODEIP_/${ips[$((n-1))]}/g \
-            | sed s%_NODELIST_%$nodelist%g \
+            | sed "s/_NODEIP_/$ip/g" \
+            | sed "s%_NODELIST_%$nodelist%g" \
                   > $qd/tm.conf
 
         cp genesis.json $qd/genesis.json
@@ -111,7 +134,7 @@ function finish_configure_nodes() {
 
         # Generate Quorum-related keys (used by Constellation)
         docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/constellation-enclave-keygen /qdata/keys/tm /qdata/keys/tma < /dev/null > /dev/null
-        echo 'Node '$n' public key: '`cat $qd/keys/tm.pub`
+        echo_yellow 'Node '$n' public key: '`cat $qd/keys/tm.pub`
 
         cp templates/start-node.sh $qd/start-node.sh
         chmod 755 $qd/start-node.sh
@@ -122,68 +145,97 @@ function finish_configure_nodes() {
 }
 
 # create the docker-compose file
-# @1 \the node ip array
+# @1 \the image name
+# @2 \the node ips
+# @3 \the node ports
+# @4 \the subnet of node
 function create_compose_file() {
-    ips=$1
+
+    local_image=$1
+    local_ips=$2
+    local_ports=$3
+    local_subnet=$4
 
     cat > docker-compose.yml <<EOF
-    version: '2'
-    services:
+version: '2'
+services:
 EOF
 
     n=1
-    for ip in ${ips[*]}
+    for ip in ${local_ips[*]}
     do
         qd=qdata_$n
 
-            cat >> docker-compose.yml <<EOF
-            node_$n:
-            image: $image
-            volumes:
-            - './$qd:/qdata'
-            networks:
+        cat >> docker-compose.yml <<EOF
+    node_$n:
+        image: $image
+        volumes:
+        - './$qd:/qdata'
+        networks:
             quorum_net:
-            ipv4_address: '$ip'
-            ports:
-            - $((n+22000)):8545
-            user: '$uid:$gid'
+                ipv4_address: '$ip'
+                ports:
+                - ${ports[$(expr $n - 1)]}:8545
+        user: '$uid:$gid'
 EOF
-
             let n++
     done
 
     cat >> docker-compose.yml <<EOF
 
-    networks:
+networks:
     quorum_net:
-    driver: bridge
-    ipam:
-    driver: default
-    config:
-    - subnet: $2
+        driver: bridge
+        ipam:
+            driver: default
+            config:
+                - subnet: $local_subnet
 EOF
 }
 
-## ./start.sh image_name network_segment ip1:port1 ip2:port2 ip3:port3
+## ./start.sh image_name network_segment ip1:port1 ip2:port2 ip3:port3 
 
 args=("$@")
 args_len=$#
+
+if [ "$args_len" -eq 0 ]; then
+    echo_yellow "usage: ./start.sh image_name subnet ip1:port1 ip2:port2 ip3:port3"
+    echo_yellow "eg:    ./start.sh quorum 172.13.0.0/16 172.13.0.4:2000 172.13.0.5:2001 172.13.0.6:2002"
+    exit
+fi
+
 image=${args[0]}
 network_segment=${args[1]}
 nodes_num=$(expr $args_len - 2)
 nodes=()
+ips=()
+ports=()
 
 for index in $(seq 0 $(expr $nodes_num - 1)); do
-    echo ${args[$(expr $index + 2)]}
     nodes[$index]=${args[$(expr $index + 2)]}
 done
 
-echo image is: $image
-echo network_segment: ${args[2]}
-echo nodes number: $nodes_num
+n=0
+for node in ${nodes[*]}; do
+    IFS=':' read -ra IP <<< $node
+    ips[$n]=${IP[0]}
+    ports[$n]=${IP[1]}
+    let n++
+done
 
-#create_nodes_directory $nodes_num
-#create_static_nodes_json $nodes_num
-#create_account_and_genesis $nodes_num
-finish_configure_nodes "${nodes[*]}"
-create_compose_file "${nodes[*]}" $network_segment
+
+printf "%-20s %-15s" image: $image
+printf "\n"
+printf "%-20s %-15s" network: ${args[1]}
+printf "\n"
+printf "%-20s %-15s" nodes_number: $nodes_num
+printf "\n"
+printf "%-20s %-15s" ips: ${ips[*]}
+printf "\n"
+printf "%-20s %-15s" ports: ${ports[*]}
+
+create_nodes_directory $nodes_num
+create_static_nodes_json $nodes_num
+create_account_and_genesis $nodes_num
+finish_configure_nodes "${ips[*]}"
+create_compose_file $image "${ips[*]}" "${ports[*]}" $network_segment
